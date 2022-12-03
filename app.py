256e771f9e097a8f3b74ff5b297ae719aee8d135
danielkellen6 <danielkellen6@gmail.com>
2022-12-03 16:50:44 -0600

    Last update

--- app.py
+++ app.py
@@ -11,13 +11,12 @@ import spacy
 import pickle as pkl
 from sentence_transformers import SentenceTransformer, util
 import torch
-from PIL import Image
+
 
 st.image('miami.jpg')
 
 header = st.container()
-dataset = st.container()
-search = st.container()
+
 
 with open("miami_df.pkl", "rb") as file1: # "rb" because we want to read in binary mode
     df = pkl.load(file1)
@@ -36,12 +35,9 @@ embedder = load_model()
 with header:
     st.title("Miami Hotel Search")
     st.markdown("As Will Smith so eloquently stated: *Welcome to Miami - Bienvenidos a Miami*")
-# with search:
 
-# embedder = SentenceTransformer('all-MiniLM-L6-v2')
+
 query = st.text_input("What are you looking for in a hotel?",'   ')
-
-# query_embeddings = embedder.encode(queries,show_progress_bar=False)
 top_k = min(1, len(corpus))
 query_embedding = embedder.encode(query, convert_to_tensor=True)
 
@@ -57,7 +53,7 @@ for score, idx in zip(top_results[0], top_results[1]):
     st.header("The best hotel for your stay: ")
     st.write(Hotel)
     st.header("What other guests had to say:")
-    st.markdown(Summary)
+    st.write(Summary)
 
 
 
